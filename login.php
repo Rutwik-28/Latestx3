0d85984672afb798521dc4c0a324c2bb3e15ba4e
Rutwik-28 <rutwikkarande28@gmail.com>
2020-12-16 05:05:05 +0530

    proj

---
+++ login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login Page</title>
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link href="ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+
+
+
+</head>
+
+<body>
+    <!----------- NAVBAR DIV -------------->
+
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="homepage.php"><img src="img/cricketlogo.png" alt=""></a>
+            </div>
+            <div class="collapse navbar-collapse" id="navbar-collapse-man">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#">LOGIN</a></li>
+                    <li><a href="#">ABOUT US</a></li>
+                    <li><a href="#">SEARCH</a></li>
+                    <li><a href="#">DISPLAY</a></li>
+                    <!--mahit nahi ata kahi hyabadal-->
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                            aria-expanded="false"><i class="glyphicon glyphicon-cog"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li class="divider"></li>
+                            <li><a href="../Profile/profile.php"><i class="glyphicon glyphicon-user"></i>&nbsp;
+                                    Profile</a></li>
+                            <li class="divider"></li>
+                            <li><a href="../Profile/logout.php"><i class="glyphicon glyphicon-log-out"></i>&nbsp;
+                                    Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Login Form</h2>
+
+                <form action="" method="post">
+
+                    <h2>Login</h2>
+
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input type="text" name="user" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="Password" name="password" class="form-control">
+                    </div>
+
+                    <button class="btn btn-primary my-nav"> Login </button>
+                    <p>If new user?<button class="btn btn-primary my-nav"><a href="registration.php"> Register
+                            </a></button></p>
+
+
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
+
+    
+
+
+</body>
+
+
+
+
+
+</html>
---
+++ script.js
@@ -0,0 +1,110 @@
+function validationcontactus(){
+
+    // <form class="contact-bg" id="contact-form" name="contact" method="post" >
+    // <input type="text" name="name" class="form-control" placeholder="Your Name">
+    // <input type="email" name="email" class="form-control" placeholder="Your E-mail">
+    // <input type="text" name="phone" class="form-control" placeholder="Phone Number">
+    // <textarea name="message" class="form-control" placeholder="Your Message" style="height:120px"></textarea>
+    // <button id="submit" type="submit" name="submit" class="btn btn-glance">Send</button>
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length < 5){
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.innerHTML = text;
+      return false;
+    }
+   
+    if(isNaN(phone) || phone.length != 10){
+      text = "Please Enter valid Phone Number";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 140){
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+  }
+
+
+
+  ///amol code
+
+
+  function clearErrors(){
+
+    errors = document.getElementsByClassName('formerror');
+    for(let item of errors)
+    {
+        item.innerHTML = "";
+    }
+
+
+}
+
+
+function seterror(id, error){
+    //sets error inside tag of id 
+    element = document.getElementById(id);
+    element.getElementsByClassName('formerror')[0].innerHTML = error;
+
+}
+
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+
+    //perform validation and if validation fails, set the value of returnval to false
+    var username = document.forms['myForm']["username"].value;
+    if (username.length<5){
+        seterror("username", "*Length of name is too short");
+        returnval = false;
+    }
+
+    if (username.length == 0){
+        seterror("username", "*Length of name cannot be zero!");
+        returnval = false;
+    }
+
+    var email = document.forms['myForm']["email"].value;
+    if (email.length>15){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+
+    var number = document.forms['myForm']["number"].value;
+    if (number.length != 10){
+        seterror("number", "*Phone number should be of 10 digits!");
+        returnval = false;
+    }
+
+    var password = document.forms['myForm']["password"].value;
+    if (password.length < 6){
+
+        // Quiz: create a logic to allow only those passwords which contain atleast one letter, one number and one special character and one uppercase letter
+        seterror("password", "*Password should be atleast 6 characters long!");
+        returnval = false;
+    }
+
+    // var cpassword = document.forms['myForm']["cpass"].value;
+    // if (cpassword != password){
+    //     seterror("cpassword", "*Password and Confirm password should match!");
+    //     returnval = false;
+    // }
+
+    return returnval;
+}
